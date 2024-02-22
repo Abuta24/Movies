c254f5ee75962b1fb63a89349f6aff166c762aa7
Abuta24 <s.abutidze2008@gmail.com>
2024-02-22 19:33:09 +0400

    Movies

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ function App() {
   return (
     <>
       <div className='header'>
-        <Link to={'/'}><img src="Movie.svg" alt="" /></Link>
+        <Link to={'/'}><img className='witeli' src="Movie.svg" alt="" /></Link>
         <nav>
-          <Link to={'/'}><img src="icon-nav-home.svg" alt="" /></Link>
-          <Link to={'/movies'}><img src="icon-nav-movies.svg" alt="" /></Link>
-          <Link to={'/tvseries'}><img src="icon-nav-tv-series.svg" alt="" /></Link>
+          <Link to={'/'}><img className='zoma' src="icon-nav-home.svg" alt="" /></Link>
+          <Link to={'/movies'}><img className='zoma' src="icon-nav-movies.svg" alt="" /></Link>
+          <Link to={'/tvseries'}><img className='zoma' src="icon-nav-tv-series.svg" alt="" /></Link>
         </nav>
-        <img src="Oval.svg" alt="" />
+        <img className='nigga' src="Oval.svg" alt="" />
       </div>
       <Routes>
         <Route path="/" element={<Home/>}/>
